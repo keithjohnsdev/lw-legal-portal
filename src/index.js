ed4f108bf855806d1f73d4c3e43f264767b6b64d
keithjohnsdev <keithjohnsdev@gmail.com>
2022-10-30 13:15:29 -0400

    gh pages test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <BrowserRouter>
+    <BrowserRouter basename="/lw-legal-portal">
       <App id="app" />
     </BrowserRouter>
   </ApolloProvider>
