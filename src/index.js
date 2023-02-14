dc541bf3ac3f27c3da7e79d7d0fa3d41680a69ba
Keith Johns <keithjohnsdev@gmail.com>
2023-02-14 16:09:18 -0500

    gh pages deploy test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import client from "./ApolloClient";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <BrowserRouter>
+    <HashRouter>
       <App id="app" />
-    </BrowserRouter>
+    </HashRouter>
   </ApolloProvider>
 );
